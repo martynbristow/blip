95bfb7a3394271b26222f35d2a8b0ba08384a691
NeechBear <nicolaw@tfb.net>
2016-09-25 23:25:18 +0100

    Add first step to control over release versions.

--- build.sh
+++ build.sh
@@ -10,31 +10,46 @@
 set -euxo pipefail
 
 main () {
+    # Version information for releases spewed in a couple of different
+    # places for Debs and RPMs, so this argument is mostly meaningless
+    # at the moment. It needs to be tied in to git release tags anyway.
+    local version="${1:-0.01}"
+    local release="${2:-1}"
+
     local base="$(readlink -f "$(dirname "$0")")"
     [[ -e "$base/blip.bash" ]]
+    source "$base/blip.bash"
 
-    local version="0.01"
     local build_dir="$base/blip-${version}/"
 
     rm -Rf --one-file-system --preserve-root "$build_dir" \
         *.deb *.changes *.dsc *.build *.gz *.rpm
 
+    # Build tarball.
     mkdir -p "$build_dir"
     rsync -av --files-from="$base/MANIFEST" "$base" "$build_dir"
     tar -C "$base" -zcvf "$base/blip-${version}.tar.gz" "$(basename "$build_dir")"
     cp "$base/blip-${version}.tar.gz" "$base/blip_${version}.orig.tar.gz"
 
-    pushd "$build_dir"
-    debuild -us -uc
-    popd
+    # Build Deb package.
+    if is_in_path "debuild" "dpkg-deb" ; then
+        pushd "$build_dir"
+        debuild -us -uc
+        popd
+        dpkg-deb -I "blip_${version}_all.deb"
+        dpkg-deb -c "blip_${version}_all.deb"
+    fi
 
-    local deb="$base/blip_${version}_all.deb"
-    [[ -e "$deb" ]] && sudo dpkg -i "$deb"
-
-    rpmbuild -ba blip.spec \
-        --define "_sourcedir $base" \
-        --define "_rpmdir $base" \
-        --define "_build_name_fmt %%{NAME}-%%{VERSION}-%%{RELEASE}.%%{ARCH}.rpm"
+    # Build RPM package.
+    if is_in_path "rpmbuild" "rpm" ; then
+        rpmbuild -ba blip.spec \
+            ${version:+--define "version $version"} \
+            ${release:+--define "release $release"} \
+            --define "_sourcedir $base" \
+            --define "_rpmdir $base" \
+            --define "_build_name_fmt %%{NAME}-%%{VERSION}-%%{RELEASE}.%%{ARCH}.rpm"
+        rpm -qlpiv "$base/blip-${version}-${release}.noarch.rpm"
+    fi
 }
 
 main "$@"
