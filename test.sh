2ace52dd67519f3e99cb2859863146c73bb1dc89
NeechBear <nicolaw@tfb.net>
2016-10-08 22:17:44 +0100

    Renamed _handler to _stack for brevity and clarity

--- test.sh
+++ test.sh
@@ -15,25 +15,28 @@ echo "${!ANSI[@]}"
 echo "${ANSI_FG_YELLOW}${ANSI_BOLD_ON}Hello world.${ANSI_RESET}"
 echo "${ANSI[blink]}${ANSI[bg_white]}${ANSI[bold]}${ANSI[red]}${ANSI[underline]}Hello world.${ANSI[reset]}"
 
-push_trap_handler "echo 'hello world'" INT
-push_trap_handler "echo 'foo bar'" INT HUP
-push_trap_handler "echo 'this will not appear'" INT HUP
-pop_trap_handler INT
-get_trap_handler INT
-set_trap_handler "echo 'woop woop'" INT
-get_trap_handler INT
-get_trap_handler HUP
-unset_trap_handler HUP
-get_trap_handler HUP
-push_trap_handler 'for ((x=0; x<=10; x++)) ; do echo " >> x=$x << "; done' INT HUP
-push_trap_handler "echo 'The final countdown.'" INT
+push_trap_stack "echo 'hello world'" INT
+push_trap_stack "echo 'foo bar'" INT HUP
+push_trap_stack "echo 'this will not appear'" INT HUP
+pop_trap_stack INT
+get_trap_stack INT
+set_trap_stack "echo 'woop woop'" INT
+get_trap_stack INT
+get_trap_stack HUP
+unset_trap_stack HUP
+get_trap_stack HUP
+push_trap_stack 'for ((x=0; x<=10; x++)) ; do echo " >> x=$x << "; done' INT HUP
+push_trap_stack "echo 'The final countdown.'" INT
 
 echo "${ANSI[bold]}${ANSI[cyan]}Try pressing Control-C to trigger a SIGINT trap handler stack.${ANSI[reset]}"
-for ((i=0; i<=10; i++)); do
+for ((i=6; i>=0; i--)); do
     echo "$i"
     sleep 1
 done
 echo "${ANSI[bold]}${ANSI[red]}Too late; better luck next time!${ANSI[reset]}"
+
+set_trap_stack "echo 'Interrupted by user input.'" INT
+set_trap_stack "echo 'Interrupted by signal.'" HUP
 
 rc_to_colour () {
     if [[ ${1:-} -eq 0 ]] ; then
