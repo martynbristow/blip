d07c3b7d155fcd8b2180e7d39c2cb4780d3a1041
NeechBear <nicolaw@tfb.net>
2016-10-25 23:06:56 +0100

    test

--- .hooks/pre-commit.sh
+++ .hooks/pre-commit.sh
@@ -1,16 +1,7 @@
 # pre-commit.sh
-<<<<<<< Updated upstream
 #git stash -q --keep-index
 make test
 #RESULT=$?
 #git stash pop -q
 #[ $RESULT -ne 0 ] && exit $RESULT
 #exit 0
-=======
-git stash -q --keep-index
-make test
-RESULT=$?
-git stash pop -q
-[ $RESULT -ne 0 ] && exit $RESULT
-exit 0
->>>>>>> Stashed changes
