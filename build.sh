51688c621377bd14ca60c9aeb3ca60bd438254a3
NeechBear <nicolaw@tfb.net>
2016-10-25 21:00:34 +0100

    Changed indenting and split up script for a smaller and easier to read main()

--- build.sh
+++ build.sh
@@ -4,136 +4,152 @@
 #
 # Copyright (c) 2016 Nicola Worthington
 #
-# Temporary script while I'm still learning about Debian packaging best
-# practices and the like. This script isn't really intended for general
-# consumption. I intend to automate the creation of release tarballs
-# and packages so that you don't have to do this kind of stuff.
-#
-# https://nicolaw.uk/DebianPackaging
-# http://www.rpm.org/max-rpm/s1-rpm-pgp-signing-packages.html
 
-set -vxueo pipefail
+set -ueo pipefail
+shopt -s checkwinsize
 umask 0077
 
-main () {
-    declare base
-    base="$(readlink -f "$(dirname "$0")")"
-    declare -x pkg="blip"
+_mark () {
+  declare msg="$*"
+  declare width=${COLUMNS:-80}
+  declare len=$(( width - ${#msg} ))
+  if [[ $len -lt $width ]] ; then
+    len=$(( len - 4 ))
+  fi
+  echo -n "${ANSI[bold]}${ANSI[yellow]}"
+  printf "=%.0s" {1..5}
+  echo -n "${msg:+[ $msg ]}"
+  printf "=%.0s" $(seq 1 $(( len - 5 )))
+  echo "${ANSI[reset]}"
+}
 
-    [[ -e "$base/${pkg}.bash" ]]
-    source "$base/${pkg}.bash"
-
-    # Version information for releases spewed in a couple of different
-    # places for Debs and RPMs, so this argument is mostly meaningless
-    # at the moment. It needs to be tied in to git release tags anyway.
-    declare dch_version_full
-    dch_version_full="$(egrep -o "^${pkg} \([0-9]+\.[0-9]+(-[0-9]+)?\) " "$base/debian/changelog" | egrep -o '[0-9]+\.[0-9]+(-[0-9]+)?' | head -1)"
-    declare -x dch_version="${dch_version_full%-*}"
-    declare -x dch_release="${dch_version_full#*-}"
-    declare -x version="${1:-$dch_version}"
-    declare -x release="${2:-$dch_release}"
-    release="${release:-1}"
-
-    [[ "$version" =~ ^[0-9]+\.[0-9]+$ ]]
-    is_int "$release"
-
-    declare -x build_base="$base/build"
-    declare -x build_dir="$build_base/${pkg}-${version}"
-    declare -x release_dir="$base/release/${pkg}-${version}${release:+-$release}"
-
-    # We should sign stuff if we're the author.
-    declare rpmbuild_extra_args
-    declare debuild_extra_args
-    declare gpg_keyid
-    if [[ "$(hostid)" = "007f0101" ]] && [[ "$(get_username)" = "nicolaw" ]] ; then
-        gpg_keyid="6393F646"
-        debuild_extra_args="-k${gpg_keyid} -S"
-        rpmbuild_extra_args="--sign"
-        while read -r line ; do
-            if ! grep -q "^$line$" ~/.rpmmacros ; then
-                echo "$line" >> ~/.rpmmacros
-            fi
-        done <<RPMMACROS
+_prepare_gpg_signing () {
+  gpg_keyid="6393F646"
+  debuild_extra_args="-k${gpg_keyid} -S"
+  rpmbuild_extra_args="--sign"
+  while read -r line ; do
+    if ! grep -q "^$line$" ~/.rpmmacros ; then
+      echo "$line" >> ~/.rpmmacros
+    fi
+  done <<RPMMACROS
 %_signature gpg
 %_gpg_name $(get_gecos_name)
 %_gpgbin /usr/bin/gpg
 RPMMACROS
+}
+
+_render_documentation () {
+  pod2man \
+    --name="BLIP.BASH" \
+    --release="${pkg}.bash $version" \
+    --center="${pkg}.bash" \
+    --section=3 \
+    --utf8 "$base/${pkg}.bash.pod" > "$base/${pkg}.bash.3"
+  if is_in_path "markdown" ; then
+    markdown "$base/README.md"
+  else
+    echo "Missing 'markdown' package is required to build README.html from README.md."
+  fi > "$base/README.html"
+}
+
+_build_deb_packages () {
+  _debuild () {
+    declare release_dir="$1"
+    declare debuild_extra_args="${2:-}"
+    declare debian_orig_tar="${release_tarball%.tar.gz}.orig.tar.gz"
+    debian_orig_tar="${debian_orig_tar//-/_}"
+
+    pushd "$build_dir"
+    cp -v "$release_tarball" "$debian_orig_tar"
+    debuild -sa ${debuild_extra_args:- -us -uc}
+    mkdir -pv "$release_dir"
+    mv -v -- "$build_base"/*.{dsc,changes,build,debian.tar.gz,orig.tar.gz} "$release_dir"
+    popd
+
+    if stat -t "$build_base"/*.deb >/dev/null 2>&1 ; then
+      mv -v -- "$build_base"/*.deb "$release_dir"
+      _mark DEB Package Information
+      dpkg-deb -I "$release_dir/${pkg}_${version}${release:+-$release}_all.deb"
+      dpkg-deb -c "$release_dir/${pkg}_${version}${release:+-$release}_all.deb"
+      _mark
     fi
+  }
 
-    # Generate some Groff man pages from the POD source.
-    pod2man \
-        --name="BLIP.BASH" \
-        --release="${pkg}.bash $version" \
-        --center="${pkg}.bash" \
-        --section=3 \
-        --utf8 "$base/${pkg}.bash.pod" > "$base/${pkg}.bash.3"
+  _debuild "$release_dir"
+  [[ -n "$debuild_extra_args" ]] && _debuild "$release_dir/deb-src" "$debuild_extra_args"
+}
 
-    if is_in_path "markdown" ; then
-        markdown "$base/README.md"
-    else
-        echo "Missing 'markdown' package is required to build README.html from README.md."
-    fi > "$base/README.html"
+_build_rpm_packages () {
+  rpmbuild -ba "$base/${pkg}.spec" \
+    ${rpmbuild_extra_args} \
+    ${pkg:+--define "name $pkg"} \
+    ${version:+--define "version $version"} \
+    ${release:+--define "release $release"} \
+    --define "_sourcedir $build_base" \
+    --define "_rpmdir $release_dir" \
+    --define "_build_name_fmt %%{NAME}-%%{VERSION}-%%{RELEASE}.%%{ARCH}.rpm"
+  _mark RPM Package Information
+  rpm -qlpiv "$release_dir/${pkg}-${version}${release:+-$release}.noarch.rpm"
+  _mark
+}
 
-    # Scan for missing documentation.
-    declare -x missing_func_docs=""
-    while read -r function ; do
-        function="${function%% *}"
-        if ! grep -q "^=head2 $function " "$base/${pkg}.bash.pod" ; then
-            missing_func_docs="${missing_func_docs:+$missing_func_docs }${function%% *}"
-        fi
-    done < <(egrep -o '^[a-z_]+\ \(\)' "$base/${pkg}.bash" | sort -u)
+main () {
+  declare base
+  base="$(readlink -f "${BASH_SOURCE[0]%/*}")"
+  declare pkg="blip"
 
-    # Build tarball.
-    rm -Rf --one-file-system --preserve-root "$build_base"
-    mkdir -p "$build_dir" "$release_dir"
-    rsync -av --exclude=".*" --exclude="build/" --exclude="release/" "${base%/}/" "${build_dir%}/"
-    tar -C "$build_base" -zcvf "$build_base/${pkg}-${version}.tar.gz" "$(basename "$build_dir")"
-    cp -v "$build_base/${pkg}-${version}.tar.gz" "$release_dir"
+  [[ -e "$base/${pkg}.bash" ]]
+  BLIP_ANSI_VARIABLES=true
+  source "$base/${pkg}.bash"
 
-    # Build Deb package.
-    if is_in_path "debuild" "dpkg-deb" ; then
-        _build_deb () {
-            declare release_dir="$1"
-            declare debuild_extra_args="${2:-}"
+  # TODO(nicolaw): Pull version information from git tags instead.
+  declare dch_version_full
+  dch_version_full="$(egrep -o "^${pkg} \([0-9]+\.[0-9]+(-[0-9]+)?\) " "$base/debian/changelog" | egrep -o '[0-9]+\.[0-9]+(-[0-9]+)?' | head -1)"
+  declare dch_version="${dch_version_full%-*}"
+  declare dch_release="${dch_version_full#*-}"
+  declare version="${1:-$dch_version}"
+  declare release="${2:-$dch_release}"
+  release="${release:-1}"
 
-            cp -v "$build_base/${pkg}-${version}.tar.gz" "$build_base/${pkg}_${version}.orig.tar.gz"
-            debuild -sa ${debuild_extra_args:- -us -uc}
-            mkdir -pv "$release_dir"
-            mv -v -- "$build_base"/*.{dsc,changes,build,debian.tar.gz,orig.tar.gz} "$release_dir"
+  [[ "$version" =~ ^[0-9]+\.[0-9]+$ ]]
+  is_abs_int "$release"
 
-            if stat -t "$release_dir"/*.deb >/dev/null ; then
-                mv -v -- "$build_base"/*.deb "$release_dir"
-                dpkg-deb -I "$release_dir/${pkg}_${version}${release:+-$release}_all.deb"
-                dpkg-deb -c "$release_dir/${pkg}_${version}${release:+-$release}_all.deb"
-            fi
-        }
+  # Declare build and release paths.
+  declare build_base="$base/build"
+  declare build_dir="$build_base/${pkg}-${version}"
+  declare release_dir="$base/release/${pkg}-${version}${release:+-$release}"
+  declare release_tarball="$build_base/${pkg}-${version}.tar.gz"
 
-        pushd "$build_dir"
-        _build_deb "$release_dir"
-        [[ -n "$debuild_extra_args" ]] && _build_deb "$release_dir/deb-src/" "$debuild_extra_args"
-        popd
-    fi
+  # Sign with the authors key.
+  declare rpmbuild_extra_args
+  declare debuild_extra_args
+  declare gpg_keyid
+  if [[ "$(hostid)" = "007f0101" ]] && [[ "$(get_username)" = "nicolaw" ]] ; then
+    _prepare_gpg_signing
+  fi
 
-    # Build RPM package.
-    if is_in_path "rpmbuild" "rpm" ; then
-        rpmbuild -ba "$base/${pkg}.spec" \
-            ${rpmbuild_extra_args} \
-            ${pkg:+--define "name $pkg"} \
-            ${version:+--define "version $version"} \
-            ${release:+--define "release $release"} \
-            --define "_sourcedir $build_base" \
-            --define "_rpmdir $release_dir" \
-            --define "_build_name_fmt %%{NAME}-%%{VERSION}-%%{RELEASE}.%%{ARCH}.rpm"
-        rpm -qlpiv "$release_dir/${pkg}-${version}${release:+-$release}.noarch.rpm"
-    fi
+  # Generate some Groff man pages from the POD source.
+  _render_documentation
 
-    # Print a summary of any functions that need to be added to documentation.
-    if [[ -n "$missing_func_docs" ]] ; then
-        echo -e "\e[0;1;33mMissing function documentation:\e[0m $missing_func_docs"
-    fi
+  # Build tarball.
+  rm -Rfv --one-file-system --preserve-root "$build_base"
+  mkdir -pv "$build_dir" "$release_dir"
+  rsync -av --exclude=".*" --exclude="build/" --exclude="release/" "${base%/}/" "${build_dir%}/"
+  tar -C "$build_base" -zcvf "$release_tarball" "${build_dir##*/}/"
+  cp -v "$release_tarball" "$release_dir"
 
-    # List the resulting release package files.
-    ls --color -Rla "$release_dir"
+  # Build Deb package.
+  if is_in_path "debuild" "dpkg-deb" ; then
+    _build_deb_packages
+  fi
+
+  # Build RPM package.
+  if is_in_path "rpmbuild" "rpm" ; then
+    _build_rpm_packages
+  fi
+
+  # List the resulting release package files.
+  ls --color -Rla "$release_dir"
 }
 
 main "$@"
