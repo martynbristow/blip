ded45fb599b3a4094460a03951c20268fda42ff1
NeechBear <nicolaw@tfb.net>
2016-10-25 21:24:03 +0100

    Skip network tests if curl cannot probe the test url

--- tests/110_network.sh
+++ tests/110_network.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 test_110_network () {
-    assert_raises 'url_exists "http://www.google.com"' 0
-    assert 'url_http_response "http://www.google.com"' "200 OK"
-    assert 'url_http_response_code "http://www.google.com"' "200"
+    declare url="http://www.google.com"
+    declare skip=0
+    curl -ks --connect-timeout 5 "$url" >/dev/null 2>&1 || skip=$?
+
+    [[ $skip -ne 0 ]] && skip
+    assert_raises "url_exists '$url'" 0
+    [[ $skip -ne 0 ]] && skip
+    assert "url_http_response '$url'" "200 OK"
+    [[ $skip -ne 0 ]] && skip
+    assert "url_http_response_code '$url'" "200"
 
     assert_end "${BASH_SOURCE[0]##*/}"
 }
